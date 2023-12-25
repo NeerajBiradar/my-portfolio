f5b6b8c2c4ff517f7c32bd1fff78ee9ec4af61db
Neeraj Biradar <Neerajssrnb2003@gmail.com>
2023-12-25 22:46:11 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
 }
 
 module.exports = nextConfig
-
-
-// next.config.js
-const withPlugins = require('./node_modules/next-compose-plugins');
-const withImages = require('./node_modules/next-images');
-
-module.exports = withPlugins([withImages], {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next',
-          name: 'static/media/[name].[hash].[ext]',
-        },
-      },
-    });
-
-    return config;
-  },
-});
